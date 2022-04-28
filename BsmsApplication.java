20e16a74e9076facf1ce03df70306d64d39aff90
Gabriela Felix <99824573+gabifbdev@users.noreply.github.com>
2022-04-28 00:11:22 -0300

    Add files via upload

---
+++ BsmsApplication.java
@@ -0,0 +1,13 @@
+package com.bsms.bsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmsApplication.class, args);
+	}
+
+}
---
+++ MetasApplication.java
@@ -0,0 +1,13 @@
+package com.Metas.metas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetasApplication.class, args);
+	}
+
+}
